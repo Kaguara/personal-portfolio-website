518a9d822782a11f4455cb08772a34c5bf1243bb
Antony Kaguara <antonykaguara@Antonys-MacBook-Pro-2.local>
2023-11-17 16:16:35 +0300

    updates to header and footer in home page

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Antony Kaguara, Product Leader, 8+ years of experience',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -75,61 +75,38 @@ const config = {
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
             position: 'right',
+            label: 'Menu',
+          },
+          {
+            to: '/blog',  
+            position: 'right',
+            label: 'Blog',
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: 'light',
         links: [
           {
-            title: 'Docs',
+            title: 'Links',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/antonykaguara/',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/kaguara',
+              },
+              {
+                label: 'Resume (CV)',
+                href: 'https://drive.google.com/file/d/11_wX5CijRe0xBRk_xyaXMZfASj7XUovJ/view?usp=sharing',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()}, Antony Kaguara's personal portfolio website. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
